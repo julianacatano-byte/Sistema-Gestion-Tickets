4f4dbaafd0f24fb73dc043e67b316caf7e3e0a61
juliana <juliana.catano@autonoma.edu.co>
2026-07-04 19:18:17 -0500

    fix: revertir Main.java al original sin Vistas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-import Vistas.VistaLogin;
-
-import javax.swing.*;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    SwingUtilities.invokeLater(() -> new VistaLogin().setVisible(true));
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
 }
