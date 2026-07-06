d471271fd0b25e2cfe299002d8d69b6a198c468f
Darihana otero <Dari200301@outlook.com>
2026-07-05 19:14:21 -0500

    Implementar estados de tickets

--- src/Modelos/Ticket.java
+++ src/Modelos/Ticket.java
@@ -1,10 +1,15 @@
 package Modelos;
 
+import java.util.Stack;
+
 public class Ticket {
+
     private String nombreCliente;
     private String asunto;
     private String prioridad;
     private String estado;
+
+    private Stack<String> historialEstados = new Stack<>();
 
     public Ticket(String nombreCliente, String asunto, String prioridad) {
         this.nombreCliente = nombreCliente;
@@ -30,6 +35,47 @@ public class Ticket {
     }
 
     public void setEstado(String estado) {
-        this.estado = estado;
+        cambiarEstado(estado);
     }
+
+    public void cambiarEstado(String nuevoEstado) {
+
+        if (historialEstados.isEmpty()) {
+            historialEstados.push(estado);
+        }
+
+        estado = nuevoEstado;
+        historialEstados.push(estado);
+    }
+
+    public void mostrarHistorial() {
+
+        if (historialEstados.isEmpty()) {
+            System.out.println("No hay historial de estados.");
+            return;
+        }
+
+        System.out.println("Historial del ticket:");
+
+        for (String estado : historialEstados) {
+            System.out.println(estado);
+        }
+    }
+
+    public void deshacerEstado() {
+
+        if (historialEstados.size() <= 1) {
+            System.out.println("No hay estados para deshacer.");
+            return;
+        }
+        historialEstados.pop();
+        estado = historialEstados.peek();
+
+        System.out.println("Estado actual: " + estado);
+    }
+
+    public Stack<String> getHistorialEstados() {
+        return historialEstados;
+    }
+
 }
